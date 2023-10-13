7c24def07149279de77ed0543f6665122780ccdc
Ayoub_Eladani <ayoub.eladani@gmail.com>
2023-10-13 11:24:40 +0100

    commit add category

---
+++ backend/routes/categoryRoute.js
@@ -0,0 +1,24 @@
+const categoryController = require("../controllers/categoryController");
+const { body } = require("express-validator") ;
+const express = require('express')
+const categoryRouter = express.Router()
+
+//! Create new category 
+categoryRouter.post(
+    "/categories",
+    [
+      body("category_name")
+        .trim()
+        .notEmpty().withMessage("the category name is required") ,
+      body("active")
+        .trim()
+        .notEmpty().withMessage("the category status is required")
+        .isBoolean().withMessage("the category status value is required") ,
+    ],
+    categoryController.addNewCategory
+  );
+
+  categoryRouter.delete("/categories/:id" , categoryController.deleteCategory) ;
+
+  module.exports = categoryRouter
+  
