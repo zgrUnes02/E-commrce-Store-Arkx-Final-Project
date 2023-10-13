2c9a1ab90dff752cada4df48c542d3e0f73057fc
Ayoub_Eladani <ayoub.eladani@gmail.com>
2023-10-13 14:52:25 +0100

    Category completed

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -9,7 +9,7 @@ const categoryController={
             active , 
         } = req.body ;
 
-    //* Check is there is any validation problem
+    //* Check if there is any validation problem
         const errors = validationResult(req) ;
         if ( !errors.isEmpty() ) {
             return res.status(400).json(errors) ;
@@ -29,6 +29,74 @@ const categoryController={
             console.log('Something went wrong' , error) ;
         }
     } ,
+    //! Delete the category profile
+    deleteCategory : async (req , res) => {
+        const { id } = req.params ;
+        try {
+            await categoryModel.findByIdAndDelete(id) ;
+            res.status(200).json({message : 'The category has been deleted with success'}) ;        
+        }
+        catch ( error ) {
+            console.log( error ) ;
+        }
+    },
+        //! Get a category by ID
+    getCategoryById : async (req , res) => {
+        const { id } = req.params ;
+        try {
+            const category = await categoryModel.findOne({_id : id}) ;
+            res.status(200).send(category) ;
+        }
+        catch ( error ) {
+            console.log( error ) ;
+        }
+    } ,
+    // ! Updating the category's data 
+    updateCategory : async (req , res) => {
+        const { category_name , active } = req.body ;
+        const { id } = req.params ;
+        try {
+            //* Find the category that i want to update his data
+            const categoryWantToUpdate = await categoryModel.findOne({_id : id}) ;
+            //* Then update the category
+            await categoryModel.findByIdAndUpdate(categoryWantToUpdate._id , {
+                category_name : category_name ,
+                active : active ,
+            }) ;
+            res.status(200).json({message : 'The category data has been updated with success'}) ;
+        }
+        catch ( error ) {
+            console.log( error ) ;
+        }
+    } ,
+        // //! Get all the categories list
+    listingCategories : async (req , res) => {
+        try {
+            //* Paginate the categories
+            const categories = await categoryModel.paginate(
+                {} , 
+                { page : req.query.page , limit : 5 }
+            ) ;
+            res.status(200).send(categories) ;
+        }
+        catch ( error ) {
+            console.log('Something went wrong' , error) ;
+        }
+    } ,
+        // //! Search for a category
+    searchForCategory : async (req , res) => {
+        try {
+            const category = await categoryModel.paginate(
+                {category_name : req.query.name} ,
+                { name : req.query.name , page : req.query.page , limit : 5 }
+            ) ;
+            res.status(200).send(category) ;
+        }
+        catch ( error ) {
+            console.log('Something went wrong' , error) ;
+        }
+    } ,
+    
 }
 
 module.exports = categoryController ;
@@ -63,7 +131,7 @@ module.exports = categoryController ;
     //         console.log('Something went wrong' , error) ;
     //     }
     // } ,
-    // //! Get a customer by ID
+    // //! Get a category by ID
     // getCategoryById : async (req , res) => {
     //     const { id } = req.params ;
     //     try {
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,24 +1,54 @@
 const categoryController = require("../controllers/categoryController");
-const { body } = require("express-validator") ;
-const express = require('express')
-const categoryRouter = express.Router()
+const { body } = require("express-validator");
+const express = require("express");
+const categoryRouter = express.Router();
 
-//! Create new category 
+//! Create new category
 categoryRouter.post(
-    "/categories",
-    [
-      body("category_name")
-        .trim()
-        .notEmpty().withMessage("the category name is required") ,
-      body("active")
-        .trim()
-        .notEmpty().withMessage("the category status is required")
-        .isBoolean().withMessage("the category status value is required") ,
-    ],
-    categoryController.addNewCategory
-  );
+  "/categories",
+  [
+    body("category_name")
+      .trim()
+      .notEmpty()
+      .withMessage("the category name is required"),
+    body("active")
+      .trim()
+      .notEmpty()
+      .withMessage("the category status is required")
+      .isBoolean()
+      .withMessage("the category status value is required"),
+  ],
+  categoryController.addNewCategory
+);
 
-//   categoryRouter.delete("/categories/:id" , categoryController.deleteCategory) ;
+//! Delete category
+categoryRouter.delete("/categories/:id", categoryController.deleteCategory);
 
-  module.exports = categoryRouter
-  
+//! Get a category by ID
+categoryRouter.get("/categories/:id", categoryController.getCategoryById);
+
+//! Updating the category data
+categoryRouter.put(
+  "/categories/:id",
+  [
+    body("category_name")
+      .trim()
+      .notEmpty()
+      .withMessage("the category name is required"),
+    body("active")
+      .trim()
+      .notEmpty()
+      .withMessage("the category status is required")
+      .isBoolean()
+      .withMessage("the category status value is required"),
+  ],
+  categoryController.updateCategory
+);
+
+//! Get all customers
+categoryRouter.get("/categories" , categoryController.listingCategories);
+
+//! Search for a customer
+categoryRouter.get("/category" , categoryController.searchForCategory);
+
+module.exports = categoryRouter;
